984ab4518194fa258e874c60f39f55c0a1b70318
Yessica Samtani <yessicamsamtani@gmail.com>
2026-03-03 14:41:36 +1000

    Farmer Jones problem setup as per lecture problem

--- prac01.py
+++ prac01.py
@@ -1,63 +1,5 @@
 import gurobipy as gp
 
-""" v2 """
-
-#Sets
-cakes = ["Choc", "Plain"]
-ingredients = ["Eggs", "Time", "Milk"]
-
-#Data
-revenue = [4, 2]                    # revenue per cake
-availability = [30, 480, 5000]      # of ingredients
-usage = [
-            [4, 20, 250],           # usage per ingredient for chocolate cake
-            [1, 50, 200]            # usage per ingredient for plain cake
-        ]
-
-#Setup Model
-m = gp.Model("Farmer Jones")
-
-#Create variables
-X = {}
-for c in range(len(cakes)):
-    X[c] = m.addVar()
-
-#Objective (to optimize)
-m.setObjective(gp.quicksum(revenue[c]*X[c] for c in range(len(cakes))), gp.GRB.MAXIMIZE)
-
-#Add constraints
-for i in range(len(ingredients)):
-    m.addConstr(gp.quicksum(usage[c][i]*X[c] for c in range(len(cakes))) <= availability[i])
-
-#Run model
-m.optimize()
-
-#Find number of each cake
-for c in range(len(cakes)):
-    print(f"Make {X[c].x} {cakes[c]}")
-
-    # X[n].lb = lowerbound
-    # X[n].ub = upperbound
-    # X[n].obj = coefficient in objective
-    # where n is variable
-
-print(f"Revenue is {m.ObjVal}")
-
-
-
-
-
-
-
-
-
-
-""" 
-v1 
-    - hardcoded variables & constraints
-"""
-
-"""
 #Setup Model
 m = gp.Model("Farmer Jones")
 
@@ -83,4 +25,3 @@ m.optimize()
 #Find number of each cake
 print(f"Make {x1.x} chocolate") #chocolate
 print(f"Make {x2.x} plain") #plain
-"""
